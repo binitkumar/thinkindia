9f61f3260d71e40c8d788b568cca899fd54e32a8
Binit Kumar <binit.kumar.hbs@gmail.com>
2019-10-14 02:44:44 +0530

    mobile files added

---
+++ public/mobile/precache-manifest.7e3c414d9903f54194c34b95036cc0b2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "581427ba5d4329bd27aa8fdb5c77637f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "edab1ebf18d361c94c7d",
+    "url": "./static/css/2.07f4df9f.chunk.css"
+  },
+  {
+    "revision": "cfc922695e11fbcd4ade",
+    "url": "./static/css/main.9fb74d10.chunk.css"
+  },
+  {
+    "revision": "edab1ebf18d361c94c7d",
+    "url": "./static/js/2.c7b45212.chunk.js"
+  },
+  {
+    "revision": "cfc922695e11fbcd4ade",
+    "url": "./static/js/main.7572ecda.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "4186db42c8c924a29244665eedce6c71",
+    "url": "./static/media/logo.4186db42.png"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "./static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "./static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "./static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "./static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
---
+++ public/mobile/precache-manifest.98952459b31dbc70539584f979598798.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d4af39020c3b17d8856099ad7a0810",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edab1ebf18d361c94c7d",
+    "url": "/static/css/2.07f4df9f.chunk.css"
+  },
+  {
+    "revision": "ee0dc437e3b444e08c6b",
+    "url": "/static/css/main.82e50c6b.chunk.css"
+  },
+  {
+    "revision": "edab1ebf18d361c94c7d",
+    "url": "/static/js/2.c7b45212.chunk.js"
+  },
+  {
+    "revision": "ee0dc437e3b444e08c6b",
+    "url": "/static/js/main.7572ecda.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4186db42c8c924a29244665eedce6c71",
+    "url": "/static/media/logo.4186db42.png"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
