2fec4fad399f701e1fa981bab061b24b3944c5f2
Binit Kumar <binit.kumar.hbs@gmail.com>
2020-05-10 21:50:40 +0530

    Destroy added

--- app/models/call_your_friend.rb
+++ app/models/call_your_friend.rb
@@ -2,7 +2,7 @@ class CallYourFriend < ApplicationRecord
 
   has_many :called_friends
 
-  accepts_nested_attributes_for :called_friends
+  accepts_nested_attributes_for :called_friends, allow_destroy: true
 
   validates_presence_of :your_name
 end
