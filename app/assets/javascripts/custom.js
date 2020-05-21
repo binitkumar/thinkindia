783c90e6789551ff1db8fa4421bdec7673ba7a01
Binit Kumar <binit.kumar.hbs@gmail.com>
2020-05-22 02:23:19 +0530

    Basic updates

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -148,8 +148,8 @@ jQuery(document).ready(function($) {
     slices: 15, // For slice animations
     boxCols: 8, // For box animations
     boxRows: 4, // For box animations
-    animSpeed: 500, // Slide transition speed
-    pauseTime: 5000, // How long each slide will show
+    animSpeed: 5, // Slide transition speed
+    pauseTime: 500000, // How long each slide will show
     startSlide: 0, // Set starting Slide (0 index)
     directionNav: true, // Next & Prev navigation
     controlNav: false, // 1,2,3... navigation
